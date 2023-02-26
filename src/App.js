941663983503e92ce06212a6c1c3eb6329954dfc
_emanoel <emanoelmartinsv@gmail.com>
2023-02-26 14:39:36 -0300

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
+function calculaTotal(compras){
+  let total = 0;
+  compras.forEach((item)=>{
+    total += parseFloat(item.preco.replace("R$ ",""));
+  })
+  return total;
+}
 
 function App() {
+  const dados = luana;
+  const situacao = dados.ativa ? {color:"green"} : {color:"red"};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>Situação:<span style={situacao}>{dados.ativa ? " Ativa": " Inativa"}</span></p>
+      <p>Total: R$ {calculaTotal(dados.compras)}</p>
+      {calculaTotal(dados.compras) > 10000 && <p>Perdeu a noção do perigo ?</p>}
+    </>
   );
 }
 
