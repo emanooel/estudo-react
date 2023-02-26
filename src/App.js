29c80f64c38f83c44853ba1867aa615e06ebbfa6
_emanoel <emanoelmartinsv@gmail.com>
2023-02-26 14:50:35 -0300

    Trabalhando com arrays array de jogos

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
-const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-    { nome: 'Guitarra', preco: 'R$ 3500' },
-  ],
-  ativa: false,
-};
-
-function calculaTotal(compras){
-  let total = 0;
-  compras.forEach((item)=>{
-    total += parseFloat(item.preco.replace("R$ ",""));
-  })
-  return total;
-}
-
+const games = ["Half-life2","Crisys 1", "Halo", "GTA - San Andreas"];
 function App() {
-  const dados = luana;
-  const situacao = dados.ativa ? {color:"green"} : {color:"red"};
+
   return (
     <>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>Situação:<span style={situacao}>{dados.ativa ? " Ativa": " Inativa"}</span></p>
-      <p>Total: R$ {calculaTotal(dados.compras)}</p>
-      {calculaTotal(dados.compras) > 10000 && <p>Perdeu a noção do perigo ?</p>}
+      {games.map((item,index)=>(
+        <li key={item}>{item}</li>
+      ))}
     </>
   );
 }
