afc958f4ddffdb3417a62112ada79ef8accbe343
_emanoel <emanoelmartinsv@gmail.com>
2023-02-26 14:58:09 -0300

    Trabalhando com array de objetos array de jogos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-const games = ["Half-life2","Crisys 1", "Halo", "GTA - San Andreas"];
+const games = [
+  {nome:"Half-life 2", ano: 2004},
+  {nome:"Crisys 1", ano: 2008} ,
+  {nome:"Halo", ano: 2001 } ,
+  {nome:"GTA - San Andreas", ano: 2004}
+];
+
 function App() {
 
   return (
-    <>
-      {games.map((item,index)=>(
-        <li key={item}>{item}</li>
+    <ul>
+      {games.map(({nome,ano})=>(
+        <li key={nome}>{nome}, {ano}</li>
       ))}
-    </>
+    </ul>
   );
 }
 
